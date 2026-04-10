f5c1b4b32617a87ef17c873a5abfc9a474681223
Alexandra <78864768+xanv754@users.noreply.github.com>
2026-04-10 11:46:43 -0400

    feat: Add custom key mapping for Neo-tree

--- dot_config/nvim/lua/plugins/neotree.lua
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -5,22 +5,31 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
-      -- Esto asegura que el explorador no se cierre al abrir un archivo
       auto_clean_config = false,
       follow_current_file = {
-        enabled = true, -- Mantiene el foco en el archivo que estás editando
+        enabled = true,
       },
     },
     window = {
       position = "left",
       width = 30,
+      mappings = {
+        ["z"] = function(state)
+          local node = state.tree:get_node()
+          if node.type == "directory" then
+            if node:is_expanded() then
+              require("neo-tree.sources.filesystem.commands").close_node(state)
+            else
+              require("neo-tree.sources.filesystem.commands").expand_all_nodes(state, node)
+            end
+          end
+        end,
+      },
     },
-    -- El truco para que "nunca se vaya" es re-lanzarlo si detecta que se cerró un archivo
     event_handlers = {
       {
         event = "file_opened",
         handler = function(file_path)
-          -- Forzamos a que se mantenga visible después de seleccionar un archivo
           require("neo-tree.command").execute({ action = "show" })
         end,
       },
